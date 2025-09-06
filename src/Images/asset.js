f66b2e413f11adc84a0e4ef8ee3ccabda254f66a
DonCarlosz <akhuemokhanisaiah@gmail.com>
2025-09-06 14:34:34 +0100

    commit

--- src/Images/asset.js
+++ src/Images/asset.js
@@ -5,7 +5,7 @@ import nodejs from "./Node.js.svg";
 import tailwind from './Tailwind.svg'
 import mongodb from './MongoDB.svg'
 import express from './Express.svg'
-import vite from './Vite.svg'
+import vite from './vite.svg'
 
 
 
